10e2fece524aaa7460aae7f03a258b24a69cf903
Jesse Shaw <JesseShawCodes@gmail.com>
2020-04-01 16:20:55 -0400

    Controllers, Views, Dynamic Content:  Redirects, Templates, Links

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,36 @@
 class DemoController < ApplicationController
   
   layout false
-  
-  def index
+
+  def data
+    url = 'http://54.162.112.10/jsonapi/node/article'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @data = response
+    puts "hello"
+    render ('hello')
   end
+
+  def index
+    puts "index"
+    render('index')
+  end
+  
+  #/demo/hello
+  def hello
+    puts ("Testing...")
+    @array = [1, 2, 3, 4, 5]
+    render ('hello')
+  end
+
+  def other_hello
+    puts ('re-direct')
+    # redirect_to(:controller => 'demo', :action => 'index')
+    redirect_to(:action => 'index')
+  end
+
+  def lynda
+    puts ('lynda!')
+    redirect_to('https://www.lynda.com')
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/data'
+  get 'demo/hello'
+
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
+  # default route
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
